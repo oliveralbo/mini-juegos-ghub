28cc4bbd51cba69ad41af50da789d0da0eabdb62
oliveralbo <oliveralbo@hotmail.com>
2019-11-07 08:03:33 -0300

    feat adivinaNumero 2 players

--- src/games/AdivinaNumero/index.jsx
+++ src/games/AdivinaNumero/index.jsx
@@ -5,6 +5,10 @@ import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
 import TransitionsModal from "../../components/Modal";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const instructionStyle = {
   // marginLeft: "250%",
@@ -28,25 +32,42 @@ function AdivinaNumero() {
   const [userNumber, setUserNumber] = useState(0);
   const [sysNumber, setSysNumber] = useState(Math.floor(Math.random() * 100));
   const [message, setMessage] = useState(null);
-  const [trys, setTrys] = useState(0);
+  const [message2, setMessage2] = useState(null);
+  const [trysP1, setTrysP1] = useState(0);
+  const [trysP2, setTrysP2] = useState(0);
   const [openModal, setOpenModal] = useState(false);
+  const [openModal2, setOpenModal2] = useState(false);
+  const [playerActive, setPlayerActive] = useState("uno");
+  const [players, setPlayers] = React.useState("uno");
+
+  const handleChangeOption = event => {
+    setPlayers(event.target.value);
+    closeModal();
+  };
 
   const handleChange = event => {
     setUserNumber(parseInt(event.target.value));
   };
 
   const handleClick = () => {
-    setTrys(trys + 1);
+    playerActive === "uno" ? setTrysP1(trysP1 + 1) : setTrysP2(trysP2 + 1);
 
     setMessage(
       userNumber > sysNumber
         ? "el numero por adivinar es menor."
         : userNumber < sysNumber
         ? "el numero por adivinar es mayor."
-        : `ganaste, acertaste en ${trys} intentos!!!`
+        : `ganaste, acertaste en ${
+            playerActive === "uno" ? trysP1 : trysP2
+          } intentos!!!`
     );
+
+    setMessage2(
+      trysP1 > trysP2 ? "Gano el jugador 2 !!" : "Gano el jugador 1 !!"
+    );
+
     if (userNumber === sysNumber) {
-      setOpenModal(true);
+      playerActive === "uno" ? setOpenModal(true) : setOpenModal2(true);
     }
   };
 
@@ -58,18 +79,53 @@ function AdivinaNumero() {
 
   const closeModal = () => {
     setOpenModal(false);
+    setOpenModal2(false);
     setUserNumber("");
-    setTrys(0);
+    setTrysP1(0);
+    setTrysP2(0);
     setMessage(null);
     setSysNumber(Math.floor(Math.random() * 100));
+  };
+
+  const closeModal2P = () => {
+    if (playerActive === "dos") {
+      closeModal();
+    } else {
+      setOpenModal(false);
+      setOpenModal2(true);
+      setUserNumber("");
+      setPlayerActive("dos");
+      setMessage(null);
+      setSysNumber(Math.floor(Math.random() * 100));
+    }
   };
 
   return (
     <Fragment>
       <Grid container spacing={1}>
         <Grid item xs={12}>
-          <Instructions style={instructionStyle} adivinaNumero />
+          <FormControl component="fieldset">
+            <RadioGroup
+              aria-label="gender"
+              name="gender1"
+              value={players}
+              onChange={handleChangeOption}
+            >
+              <FormControlLabel
+                value="uno"
+                control={<Radio color="primary" />}
+                label="1 Jugador"
+              />
+              <FormControlLabel
+                value="dos"
+                control={<Radio />}
+                label="2 Jugadores"
+              />
+            </RadioGroup>
+          </FormControl>
 
+          <Instructions style={instructionStyle} adivinaNumero />
+          <h4>{playerActive === "uno" ? "Jugador N°1" : "Jugador N°2"} </h4>
           <h3 style={subTitleStyle}>Que número eligió el sistema ?</h3>
         </Grid>
 
@@ -89,9 +145,20 @@ function AdivinaNumero() {
         </Grid>
         <Grid item xs={12} sm={4}>
           <p>Intentos : </p>
-          <Fab color="primary" aria-label="add">
-            {trys}
-          </Fab>
+          {players === "uno" ? (
+            <Fab color="primary" aria-label="add">
+              {trysP1}
+            </Fab>
+          ) : (
+            <>
+              <Fab color="primary" aria-label="add">
+                {trysP1}
+              </Fab>
+              <Fab color="secondary" aria-label="add">
+                {trysP2}
+              </Fab>
+            </>
+          )}
         </Grid>
 
         <Grid item sm={12}>
@@ -112,8 +179,13 @@ function AdivinaNumero() {
       </Grid>
       <TransitionsModal
         handleOpen={openModal}
-        handleClose={closeModal}
+        handleClose={players === "uno" ? closeModal : closeModal2P}
         message={message}
+      />
+      <TransitionsModal
+        handleOpen={openModal2}
+        handleClose={closeModal}
+        message={message2}
       />
     </Fragment>
   );
